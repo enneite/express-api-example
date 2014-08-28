8f27ae08ce77ea9b155bd461a5264f74ab182982
etienne_lejariel <etienne.lejariel@wonderbox.fr>
2014-08-28 18:21:02 +0200

    add user schema

---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+    ,Schema = mongoose.Schema
+    ,ObjectId = Schema.ObjectId;
+
+var userSchema = new Schema({
+    createdDate: {type: Date, default: Date.now},
+    username: {type: String, required: true},
+    pwd: {type: String, required: true},
+    birthday: {type: Date, required: false},
+    status: {type: String, default:'waiting'}
+});
+
+module.exports = exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+
+  User.find(function(err, users) {
+      if(err) {
+          console.log(err.message);
+          res.send('an error occured' + err.message);
+      }
+      else {
+          res.json(users);
+      }
+  });
+
 });
 
 module.exports = router;
